2c1e5d9288eba0b4e5df9db4f1d62ba17810ff07
Michael Green <green.michael.todd@gmail.com>
2013-09-03 03:28:24 +0000

    updated file to pull heroku toolbelt

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
-sudo apt-get install -y git-core
+sudo apt-get install -y git
+sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
@@ -25,6 +26,10 @@ sudo apt-get install -y rlwrap
 sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+# Install Heroku toolbelt
+# https://toolbet.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
